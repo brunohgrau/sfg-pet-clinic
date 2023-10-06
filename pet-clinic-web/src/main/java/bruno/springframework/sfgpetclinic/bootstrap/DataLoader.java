2e30259f4469c90cd17936ab50315f3e490c4d7d
Bruno <brunohgrau99@gmail.com>
2023-10-06 18:55:28 -0300

    Enhance owners with contact info and Pets Closes #33

--- pet-clinic-web/src/main/java/bruno/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/bruno/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,5 +1,6 @@
 package bruno.springframework.sfgpetclinic.bootstrap;
 import bruno.springframework.sfgpetclinic.model.Owner;
+import bruno.springframework.sfgpetclinic.model.Pet;
 import bruno.springframework.sfgpetclinic.model.PetType;
 import bruno.springframework.sfgpetclinic.model.Vet;
 import bruno.springframework.sfgpetclinic.services.OwnerService;
@@ -7,6 +8,8 @@ import bruno.springframework.sfgpetclinic.services.PetTypeService;
 import bruno.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 
 @Component
@@ -36,12 +39,33 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Bickerl ");
+        owner1.setCity("Miami");
+        owner1.setTelephone("1234");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Miina");
+        owner1.getPets().add(mikesPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Jones");
+        owner2.setAddress("87 John ");
+        owner2.setCity("Miami");
+        owner2.setTelephone("13334");
+
+        Pet fionasCat = new Pet();
+        fionasCat.setPetType(savedCatPetType);
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setName("Zé");
+        owner2.getPets().add(fionasCat);
+
 
         ownerService.save(owner2);
 
