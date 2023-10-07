349984ee0f798cadcbb40ac500a4a088a46ce635
Bruno <brunohgrau99@gmail.com>
2023-10-06 21:04:31 -0300

    Create Speciality Map Service Impl Closes #31

---
+++ pet-clinic-data/src/main/java/bruno/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,9 @@
+package bruno.springframework.sfgpetclinic.services;
+
+import bruno.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality,Long>{
+
+
+
+}
