38db0dbc466e81bd99551471b0533f0c45fd2737
Bruno <brunohgrau99@gmail.com>
2023-10-08 21:44:27 -0300

    Menu Links are Broken  Closes #45

--- pet-clinic-web/src/main/java/bruno/springframework/sfgpetclinic/controller/IndexController.java
+++ pet-clinic-web/src/main/java/bruno/springframework/sfgpetclinic/controller/IndexController.java
@@ -8,4 +8,10 @@ public class IndexController {
     public String index(){
         return "index";
     }
+
+    @RequestMapping("/oups")
+    public String oupsHandler(){
+        return "notimplemented";
+    }
+
 }
--- pet-clinic-web/src/main/java/bruno/springframework/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/bruno/springframework/sfgpetclinic/controller/OwnerController.java
@@ -7,18 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
-
     private final OwnerService ownerService;
-
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
     }
-
-
     @RequestMapping({"","/","/index", "/index.html"})
         public String listOwners(Model model){
         model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
+
+    @RequestMapping("/find")
+    public String findOwners(){
+        return "notimplemented";
+    }
+
 
 }
