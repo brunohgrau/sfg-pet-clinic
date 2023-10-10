da1171b70ee9faed119e8e1bed1cb55005081af6
Bruno <brunohgrau99@gmail.com>
2023-10-09 21:49:51 -0300

    Convert Vets to JPA Entities Closes #37

--- pet-clinic-data/src/main/java/bruno/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/bruno/springframework/sfgpetclinic/model/Speciality.java
@@ -1,9 +1,17 @@
 package bruno.springframework.sfgpetclinic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity{
+    @Column(name = "description")
     private String description;
 
-
+    public Speciality() {
+    }
 
     public Speciality(String description) {
         this.description = description;
