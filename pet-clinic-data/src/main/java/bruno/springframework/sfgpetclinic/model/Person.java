31ef5855e43f5a6fda25e90da0f9ce553c0a4e4c
Bruno <brunohgrau99@gmail.com>
2023-10-09 21:27:07 -0300

    Convert Owners to JPA Entities Closes #36

--- pet-clinic-data/src/main/java/bruno/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/bruno/springframework/sfgpetclinic/model/Person.java
@@ -1,10 +1,14 @@
 package bruno.springframework.sfgpetclinic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity{
-
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
-
     public Person() {
     }
 
--- pet-clinic-data/src/main/java/bruno/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/bruno/springframework/sfgpetclinic/model/PetType.java
@@ -1,23 +1,25 @@
 package bruno.springframework.sfgpetclinic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
     // declarando atributos
+    @Column(name = "name")
     private String name;
 
     // construtor para instanciar objeto
     public PetType() {
     }
-
     public PetType(String name) {
         this.name = name;
     }
-
-
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
